ea50ff2c9848f4a1cae612ddab4b954e40038233
danieldami <danieldami9999@gmail.com>
2025-11-28 11:13:19 +0100

    describe

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       MONGO_URI: 'mongodb+srv://dereeves:dereeves1@cluster0.k93rlyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
       JWT_SECRET: 'mySuperSecretKeypr',
       // Frontend origin (used by CORS and redirects)
-      CLIENT_URL: 'https://dereevesfoundations.com'
+      CLIENT_URL: 'https://dereevesfoundations.com',
       RESEND_API_KEY: "re_2w24me6B_9VKfRAbncGHCaSvTsVi444JH",
       RESEND_FROM_EMAIL: "noreply@dereevesfoundations.com",
       FRONTEND_URL: "https://dereevesfoundations.com"
